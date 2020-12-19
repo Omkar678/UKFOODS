b46cd46dc0609bcd9f748ee6a9174a530ed67755
Omkar678 <76251855+Omkar678@users.noreply.github.com>
2020-12-19 18:33:56 +0530

    Add files via upload

---
+++ UKFOODS javascript.js
@@ -0,0 +1,21 @@
+function onav(){
+		document.querySelector('#msn').style.width = "250px"
+	}
+	function cnav()	{	
+		document.querySelector('#msn').style.width = "0px"
+	}
+	function unFunctional(){
+		alert("Oops! This function is currently unavailable. Please try again later")
+		try{
+			buy(price)
+		}
+         catch(err){
+         	console.log("Ha! That's the problem " + err.message)
+         }
+	}
+	alert("Hi! Welcome to UKFOODS")
+	var impro = prompt("If you have any comments, please write them hear")
+	alert("We heard that " + impro + " from you")
+	for(let i = 0;i<10;i++){
+     console.log(i)   
+	}
